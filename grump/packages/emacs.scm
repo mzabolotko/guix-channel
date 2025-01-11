1f8b18cf3d218511bc3c601289472a65a9eee78f
Михаил Заболотько <zabolotko@gmail.com>
2025-01-11 17:13:56 +0300

    added emacs-nerd-icons-dired

--- grump/packages/emacs.scm
+++ grump/packages/emacs.scm
@@ -30,3 +30,25 @@
     (description "Nerd-icons an alternative to all-the-icons.  It works on both
 GUI and terminal, and requires a nerd font installed on your system.")
     (license license:gpl3))))
+
+(define-public emacs-nerd-icons-dired
+  (let ((commit "c1c73488630cc1d19ce1677359f614122ae4c1b9"))
+  (package
+    (name "emacs-nerd-icons-dired")
+    (version "0.0.1")
+    (source
+      (origin
+      (method git-fetch)
+      (uri (git-reference
+        (url "https://github.com/rainstormstudio/nerd-icons-dired")
+        (commit commit)))
+      (file-name (git-file-name name version))
+      (sha256
+        (base32 "1iwqzh32j6fsx0nl4y337iqkx6prbdv6j83490riraklzywv126a"))))
+    (propagated-inputs
+      (list emacs-nerd-icons))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/rainstormstudio/nerd-icons-dired")
+    (synopsis "nerd-icons theme for dired")
+    (description "nerd-icons theme for dired")
+    (license license:gpl3))))
